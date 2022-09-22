6edfcf2c182cd60092c42576b95ee57334aaa3ad
Görkem Şakalak <gorkemsakalak@gmail.com>
2022-09-23 02:26:38 +0300

    Updated MainComponent.vue

---
+++ src/components/MainComponent.vue
@@ -0,0 +1,39 @@
+<script setup>
+
+const props = defineProps(["groupNames"])
+const emits = defineEmits(["getPeople"])
+const getPeople = (group) => {
+		emits("getPeople", group)
+}
+
+</script>
+
+<template>
+	
+	<div class="container">
+		<ul class="group-list">
+			<li v-for="group in props.groupNames" @click="getPeople(group)">
+				{{group}}
+				
+			</li>
+		</ul>
+			
+	</div>
+
+</template>
+
+<style scoped>
+	.group-list {
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: center;
+		align-items: center;
+		gap: 8px;
+	}
+
+	.group-list li {
+		border: 1px solid white;
+		cursor: pointer;
+		padding: 8px;
+	}
+</style>
